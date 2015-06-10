77c6527fae8ad493426047c0d854060b0a119c41
alexignat <d.alexandru.ignat@gmail.com>
2015-06-10 14:28:22 -0400

    Finished problem 3

--- exercise6.rb
+++ exercise6.rb
@@ -2,17 +2,27 @@
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
+# Method iterates through each item in the array and adds a "*"
+
 def puts_list(list)
   list.map { |x| puts "*" + x}
 end
 
 puts_list(grocery_list)
 
+# Counting number of items in the list
+
+puts "There are #{grocery_list.count} items in the list."
+
 grocery_list << "rice"
 
-puts "Added rice to grocery list"
+puts "Added rice to grocery list."
 
 puts_list(grocery_list)
+
+# Counting the number of items in the list
+
+puts "There are now #{grocery_list.count} items in the list."
 
 
 
