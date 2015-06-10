e89303f925ebe9ad40ba10a7f50e381e9fee6e1d
alexignat <d.alexandru.ignat@gmail.com>
2015-06-10 15:28:48 -0400

    Finished problem 1

--- exercise7.rb
+++ exercise7.rb
@@ -7,3 +7,15 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+# Create a method that shows puts the keys and values
+
+def show(item)
+  item.each { |key, value| puts "#{key}: #{value} studens"}
+end
+
+show(students)
+
+
+
+
