8752f40463e05e059e49d08991d1ce1fe3711120
alexignat <d.alexandru.ignat@gmail.com>
2015-06-09 22:06:30 -0400

    Version 1 temp program

--- exercise5.rb
+++ exercise5.rb
@@ -1,3 +1,20 @@
 # Temperature Conversion Program
 # Converts Fahrenheit to Celsius
 
+
+# Version 1 of temperature Program
+
+
+def change_temp(temp_f)
+	temp_c = (temp_f - 32) * 5.0/9
+	return temp_c.round(2)
+end
+
+puts "What is the temperature in Fahrenheit?"
+
+temp_f = gets.chomp.to_i
+
+puts "Converting to Celsius\n..............."
+
+puts "The temperature in Celsius is #{change_temp(temp_f)}."
+
